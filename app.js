8a81eead7292b099001bf4e69a5aed2fb5aa4c7c
MansourAlbatran <>
2021-06-03 12:20:49 +0000

    js lab

---
+++ app.js
@@ -0,0 +1,10 @@
+document.write("<p>mansoure</p>")
+alert("hi")
+
+var x = prompt("number of your gools")
+
+console.log(x)
+if(x<10){alert("not good")}
+else if(x=100){alert("great")}
+
+else{alert("good")}

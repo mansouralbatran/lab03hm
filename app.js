9b92df7c3f16f2011b51dc43f9c63874030807d4
MansourAlbatran <>
2021-06-06 12:54:43 +0000

    while and for

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-document.write("<p>mansoure</p>")
-alert("hi")
+// document.write("<p>mansoure</p>")
+// alert("hi")
 
-var x = prompt("number of your gools")
+// var x = prompt("number of your gools")
 
-console.log(x)
-if(x<10){alert("not good")}
-else if(x=100){alert("great")}
+// console.log(x)
+// if(x<10){alert("not good")}
+// else if(x=100){alert("great")}
 
-else{alert("good")}
+// else{alert("good")}
+
+var name = prompt("inter yourvaferot playecristiano orMessi ")
+
+while (name !== "cristiano" && name !== "Messi") {
+  name = prompt("inter yourvaferot player cristiano or Messi ")
+}
+var x = prompt("how will you give star for your star")
+for (i = 1; i <= x; i++) {
+  document.write('<img src="https://stgabrielshuyton.net/nursery1920/wp-content/uploads/sites/30/2019/10/star-3.png"/>')}

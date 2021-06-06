8ddaa748cbe5b2ab1924733b495c6047b1a8017b
MansourAlbatran <>
2021-06-06 14:23:05 +0000

    function

--- app.js
+++ app.js
@@ -8,12 +8,19 @@
 // else if(x=100){alert("great")}
 
 // else{alert("good")}
-
 var name = prompt("inter yourvaferot playecristiano orMessi ")
-
-while (name !== "cristiano" && name !== "Messi") {
-  name = prompt("inter yourvaferot player cristiano or Messi ")
+function namePler() {
+  while (name !== "cristiano" && name !== "Messi") {
+    name = prompt("inter yourvaferot player cristiano or Messi ")
+  }
 }
-var x = prompt("how will you give star for your star")
-for (i = 1; i <= x; i++) {
-  document.write('<img src="https://stgabrielshuyton.net/nursery1920/wp-content/uploads/sites/30/2019/10/star-3.png"/>')}
+namePler()
+
+
+var stars = prompt("how will you give star for your star")
+function showStars() {
+  for (i = 1; i <= stars; i++) {
+    document.write('<img src="https://stgabrielshuyton.net/nursery1920/wp-content/uploads/sites/30/2019/10/star-3.png"/>')
+  }
+}
+showStars()
